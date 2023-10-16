18fad9e99aa8810edee458740c2eb921dc23cb87
atlantis-l <litingchuan1016@gmail.com>
2023-10-16 15:21:27 +0800

    Modified test configuration file

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,6 @@ import type { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   testTimeout: 10000,
-  testMatch: [
-    // "**/test/transaction-preview.test.ts",
-    // "**/test/manifest-convert.test.ts",
-    // "**/test/free-xrd-from-faucet.test.ts",
-    // "**/test/radix-wallet-generator.test.ts",
-    // "**/test/xrd-transfer.test.ts",
-    "**/test/*.test.ts",
-  ],
 };
 
 export default jestConfig;
