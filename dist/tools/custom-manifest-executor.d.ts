0d2dce0b0c8609d2f5e584502118b300ef4533ee
atlantis-l <litingchuan1016@outlook.com>
2023-11-28 16:44:44 +0800

    Rebuilt dist

--- dist/tools/custom-manifest-executor.d.ts
+++ dist/tools/custom-manifest-executor.d.ts
@@ -1,9 +1,10 @@
+import { PublicKey, PrivateKey } from "@radixdlt/radix-engine-toolkit";
 import { Wallet } from "../models";
 declare class CustomManifestExecutor {
     networkId: number;
     executorWallet: Wallet;
     constructor(networkId: number, executorWallet: Wallet);
-    execute(manifestString: string, signatoryWallets: Wallet[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
-    executePreview(manifestString: string, signatoryWallets: Wallet[], currentEpoch: number): Promise<import("../models").PreviewResult>;
+    execute(manifestString: string, signerPrivateKeys: PrivateKey[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
+    executePreview(manifestString: string, signerPublicKeys: PublicKey[], currentEpoch: number): Promise<import("../models").PreviewResult>;
 }
 export { CustomManifestExecutor };
--- dist/tools/token-sender.d.ts
+++ dist/tools/token-sender.d.ts
@@ -1,4 +1,4 @@
-import { Amount } from "@radixdlt/radix-engine-toolkit";
+import { Amount, PublicKey, PrivateKey } from "@radixdlt/radix-engine-toolkit";
 import { CustomOption, Wallet, TransferInfo } from "../models";
 declare class TokenSender {
     networkId: number;
@@ -6,10 +6,10 @@ declare class TokenSender {
     feePayerWallet: Wallet;
     feeLock: string;
     constructor(networkId: number, mainWallet: Wallet);
-    sendFungibleToken(toAddress: string, tokenAddress: string, amount: Amount, message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
-    sendNonFungibleToken(toAddress: string, tokenAddress: string, nonFungibleLocalIds: string[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
-    sendTokens(toAddress: string, transferInfos: TransferInfo[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
-    sendCustom(customOptions: CustomOption[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
-    sendCustomPreview(customOptions: CustomOption[], currentEpoch: number): Promise<import("../models").PreviewResult>;
+    sendFungibleToken(toAddress: string, tokenAddress: string, amount: Amount, signerPrivateKeys: PrivateKey[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
+    sendNonFungibleToken(toAddress: string, tokenAddress: string, nonFungibleLocalIds: string[], signerPrivateKeys: PrivateKey[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
+    sendTokens(toAddress: string, transferInfos: TransferInfo[], signerPrivateKeys: PrivateKey[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
+    sendCustom(customOptions: CustomOption[], signerPrivateKeys: PrivateKey[], message: string | undefined, currentEpoch: number): Promise<import("../models/result").Result>;
+    sendCustomPreview(customOptions: CustomOption[], signerPublicKeys: PublicKey[], currentEpoch: number): Promise<import("../models").PreviewResult>;
 }
 export { TokenSender };
