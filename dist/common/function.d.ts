8d9e2e710d49d82daa8c4845b22565333feef10c
atlantis-l <litingchuan1016@outlook.com>
2024-01-30 23:58:43 +0800

    Rebuild dist

--- dist/common/function.d.ts
+++ dist/common/function.d.ts
@@ -2,9 +2,10 @@ import { PublicKey, TransactionManifest, NotarizedTransaction, CompiledNotarized
 import { GatewayApiClient } from "@radixdlt/babylon-gateway-api-sdk";
 import { PreviewResult, Result } from "../models";
 declare function selectNetwork(networkId: number): GatewayApiClient;
+declare function formatConvert(str: string, networkId: number): Promise<string>;
 declare function getCurrentEpoch(networkId: number): Promise<number>;
 declare function generateTransaction(transaction: NotarizedTransaction): Promise<CompiledNotarizedTransaction>;
 declare function processTransaction(networkId: number, f: () => Promise<NotarizedTransaction>): Promise<Result>;
 declare function previewTransaction(networkId: number, manifest: TransactionManifest, notaryPublicKey: PublicKey, signerPublicKeys: PublicKey[], blobsHex: string[], currentEpoch: number): Promise<PreviewResult>;
 declare function convertManifestTo(kind: "String" | "Parsed", manifest: TransactionManifest, networkId: number): Promise<void>;
-export { selectNetwork, getCurrentEpoch, convertManifestTo, previewTransaction, processTransaction, generateTransaction, };
+export { selectNetwork, formatConvert, getCurrentEpoch, convertManifestTo, previewTransaction, processTransaction, generateTransaction, };
