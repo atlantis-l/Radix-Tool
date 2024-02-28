890db15731205b00d5e4cd42f00792dd20557c12
atlantis-l <litingchuan1016@outlook.com>
2024-02-28 11:14:09 +0800

    Rebuild

--- dist/models/preview-result.d.ts
+++ dist/models/preview-result.d.ts
@@ -2,4 +2,12 @@ export declare interface PreviewResult {
     fee: string;
     status: string;
     errorMessage?: string;
+    feeSummary: {
+        execution_cost_units_consumed: number;
+        finalization_cost_units_consumed: number;
+    };
+    costingParameters: {
+        execution_cost_unit_limit: number;
+        finalization_cost_unit_limit: number;
+    };
 }
